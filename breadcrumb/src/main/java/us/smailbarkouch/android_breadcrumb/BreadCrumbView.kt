f8cd0508a46fb240c45a31a5ca3a7953fdc36aa3
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2022-11-18 20:56:37 +0900

    バックグラウンド描画

--- breadcrumb/src/main/java/us/smailbarkouch/android_breadcrumb/BreadCrumbView.kt
+++ breadcrumb/src/main/java/us/smailbarkouch/android_breadcrumb/BreadCrumbView.kt
@@ -29,11 +29,18 @@ class BreadCrumbView : FrameLayout {
         attrs?.let {
             val typedArray =
                 context.obtainStyledAttributes(attrs, R.styleable.BreadCrumbView, defStyleAttr, 0)
+
+            val clickable = typedArray.getBoolean(R.styleable.BreadCrumbView_clickable, true)
             val arrowDrawable =
                 typedArray.getResourceId(R.styleable.BreadCrumbView_arrow_drawable, -1)
             val textColor = typedArray.getColor(R.styleable.BreadCrumbView_text_color, -1)
             val textSize = typedArray.getColor(R.styleable.BreadCrumbView_text_size, -1)
+            val backGround =
+                typedArray.getResourceId(R.styleable.BreadCrumbView_background_drawable, -1)
             typedArray.recycle()
+
+//            breadCrumbAdapter.setClickable(clickable)
+
             if (arrowDrawable != -1) {
                 breadCrumbAdapter.setArrowDrawable(arrowDrawable)
             }
@@ -42,6 +49,9 @@ class BreadCrumbView : FrameLayout {
             }
             if (textSize != -1) {
                 breadCrumbAdapter.setTextSize(textSize)
+            }
+            if (backGround != -1) {
+                setBackgroundResource(backGround)
             }
         }
 
@@ -74,6 +84,7 @@ class BreadCrumbView : FrameLayout {
     fun setListener(listener: BreadCrumbItemClickListener) {
         breadCrumbAdapter.breadCrumbItemClickListener = listener
     }
+
     fun setArrowDrawable(arrowDrawable: Int) = breadCrumbAdapter.setArrowDrawable(arrowDrawable)
     fun setBreadCrumbItems(items: MutableList<BreadCrumb>) {
         breadCrumbAdapter.setBreadCrumbItems(items)
@@ -85,4 +96,15 @@ class BreadCrumbView : FrameLayout {
     fun getBreadCrumbItem(position: Int) = breadCrumbAdapter.getBreadCrumbItem(position)
     fun removeAllBreadCrumbItems() = breadCrumbAdapter.removeAllBreadCrumbItems()
     fun removeLastBreadCrumbItem() = breadCrumbAdapter.removeLastBreadCrumbItem()
+
+//    fun setClickable(clickable: Boolean) {
+//        // TODO recyclerViewの子すべてに clickableを反映する
+//        recyclerView.children.forEach { v ->
+//            v.isEnabled = clickable
+//        }
+//    }
+
+    fun setPosition(position: Int) {
+        TODO("IMPLEMENT THIS")
+    }
 }
